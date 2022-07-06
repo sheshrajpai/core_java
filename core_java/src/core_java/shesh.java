3e12002e022f9a1c6ad97d3d680c34acb78c4aca
Nandkumar pai <Nandkumar pai@Bamboozled>
2022-07-06 17:01:28 +0530

    commit

---
+++ core_java/src/core_java/shesh.java
@@ -0,0 +1,11 @@
+package core_java;
+
+public class shesh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+System.out.print("sheshraj");
+	}
+
+}
---
+++ core_java/src/module-info.java
@@ -0,0 +1,3 @@
+module core_java {
+	
+}
